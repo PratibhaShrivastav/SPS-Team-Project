d34091f6ad36aca077d57c5d890e6572310e48be
Chirag Thakur <chirag11032000@gmail.com>
2020-08-25 07:14:40 +0000

    Move common functions to utils.js and use Ajax for adding item to todo list

--- src/main/webapp/assets/javascript/movie_dedicated_page.js
+++ src/main/webapp/assets/javascript/movie_dedicated_page.js
@@ -3,35 +3,6 @@ const API_KEY = '28e7691b28199415eec6fd8d3e1ffd18';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 const YOUTUBE_WATCH_URL = 'https://www.youtube.com/watch?v='
 
-async function getYoutubeUrl(entityType, entityId) {
-  let response = await axios.get(`${API_BASE_URL}/${entityType}/${entityId}/videos`, {
-    params: {
-      api_key: API_KEY
-    }
-  });
-  console.log('Got Youtube Url for ', entityId, 'response: ', response);
-  return response.data.results[0].key;
-}
-
-function addToDo(entityType, entityID){
-  var form = $("<form></form>");
-  form.attr('method', 'post');
-  form.attr('action', '/mark_todo');
-  var field1 = $('<input></input>');
-  field1.attr('type', 'hidden');
-  field1.attr('name', 'EntityType');
-  field1.attr('value', entityType);
-  form.append(field1);
-  var field2 = $('<input></input>');
-  field2.attr('type', 'hidden');
-  field2.attr('name', 'EntityID');
-  field2.attr('value', entityID);
-  form.append(field2);
-  console.log(form);
-  $(document.body).append(form);
-  form.submit();
-}
-
 let movieId = $('#movie-details').data('movie-id');
 axios.get(`${API_BASE_URL}/movie/${movieId}`, {
   params: {
@@ -46,14 +17,14 @@ axios.get(`${API_BASE_URL}/movie/${movieId}`, {
 
   $('#movieName').html(`
     <center>
-      <h1 class="orangeHeading">${response.data.original_title}</h3>
+      <h1 class="orangeHeading">${response.data.title}</h3>
     </center>
   `);
 
   let youtubeUrlKey = await getYoutubeUrl("movie", movieId);
   let youtubeUrl = `${YOUTUBE_WATCH_URL}${youtubeUrlKey}`;
   $('#linkToTrailer').html(`
-    <button id = "btn btn-primary" type="button" onclick="location.href='${youtubeUrl}'">
+    <button id="btn btn-primary" type="button" onclick="location.href='${youtubeUrl}'">
       <span class="glyphicon glyphicon-play"></span>
       &nbspPlay trailer
     </button>
